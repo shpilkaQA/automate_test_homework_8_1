4a9b86a355d510edca36bd04056cab155726cc24
Anna Plotnikova <ansrpl777@gmail.com>
2020-01-06 19:14:54 +0300

    Тесты - проверка верификации

--- src/test/java/ru/netology/TestLoginIn.java
+++ src/test/java/ru/netology/TestLoginIn.java
@@ -1,91 +1,104 @@
 package ru.netology;
 
 import com.codeborne.selenide.SelenideElement;
-import com.github.javafaker.Faker;
-import jdk.nashorn.internal.runtime.Debug;
 import lombok.val;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
-import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.junit.jupiter.api.*;
-import ru.netology.mode.User;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestLoginIn {
-    /*@BeforeEach
-    void setUp() throws SQLException {
-        val faker = new Faker();
-        val runner = new QueryRunner();
-        val dataSQL = "INSERT INTO users(id, login, password) VALUES (?, ?, ?);";
-        val codeSQL = "INSERT INTO auth_codes(id, user_id, code) VALUES (?, ?, ?);";
-
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://192.168.99.100:3306/app", "app", "pass"
-                );
-        ) {
-            // обычная вставка
-            runner.update(conn, dataSQL, faker.random().nextInt(0,2000000), faker.name().username(), "pass");
-            runner.update(conn, codeSQL, faker.random().nextInt(0,2000000), ,
-                    faker.random().nextInt(0, 2000000));
-        }
-    }*/
+    private static final String WEBSITE = "http://localhost:9999";
+    private static final String URLDB = "jdbc:mysql://192.168.99.100:3306/app";
+    private static final String USER = "app";
+    private static final String PASSWORD = "pass";
 
     @AfterAll
     public static void dbClear() throws SQLException {
         val runner = new QueryRunner();
-//        val deleteUsers = "TRUNCATE TABLE users;"; //Очищаем таблицу users
-        val deleteUsers = "DELETE FROM users;"; //Очищаем таблицу users
         val deleteAuth_codes = "DELETE FROM auth_codes;";
-        val deleteCards = "DELETE FROM cards;";
-        val deleteCard_transactions = "DELETE FROM card_transactions;";
         try (
                 val conn = DriverManager.getConnection(
-                        "jdbc:mysql://192.168.99.100:3306/app", "app", "pass"
+                        URLDB, USER, PASSWORD
                 );
         ) {
-            runner.update(conn, deleteUsers);
             runner.update(conn, deleteAuth_codes);
-            runner.update(conn, deleteCards);
-            runner.update(conn, deleteCard_transactions);
+            System.out.println("Таблица auth_codes очищена");
         }
     }
-
     @Test
-    void stubTest() throws SQLException {
+    @DisplayName("Все поля заполнены верно")
+    void validTestLoginIn() throws SQLException {
+        val login = "vasya";
+        val password = "qwerty123";
         val userCode = "SELECT auth_codes.code FROM users JOIN auth_codes on users.id = auth_codes.user_id " +
-                "WHERE users.login = ?";
-        val usersSQL = "SELECT users.id, users.login, users.password, auth_codes.code " +
-                "FROM users JOIN auth_codes on users.id = auth_codes.user_id GROUP BY users.id, auth_codes.code";
+                "WHERE users.login = 'vasya';";
         val runner = new QueryRunner();
 
         try (
                 val conn = DriverManager.getConnection(
-                        "jdbc:mysql://192.168.99.100:3306/app", "app", "pass"
+                        URLDB, USER, PASSWORD
                 );
         ) {
-
-            val firstUser = runner.query(conn, usersSQL, new BeanHandler<>(User.class));
-            System.out.println(firstUser);
-
-
+            open(WEBSITE);
+            SelenideElement form = $(".form");
+            form.$("[data-test-id=login] input").setValue(login);
+            form.$("[data-test-id=password] input").setValue(password);
+            form.$(".button").click();
+            form.$("[data-test-id=code]").waitUntil(visible, 1000);
+            val code = runner.query(conn, userCode, new ScalarHandler<>());
+            form.$("[data-test-id=code] input").setValue(String.valueOf(code));
+            form.$(".button").click();
+            $("[data-test-id=dashboard]").waitUntil(visible, 1000);
         }
-
-        /*open("http://localhost:9999");
+    }
+    @Test
+    @DisplayName("Поля не заполнены")
+    void shouldErrorTestOfNull() {
+        open(WEBSITE);
         SelenideElement form = $(".form");
-        form.$("[data-test-id=name] input").setValue("ПроРоолл ОллЛлл-олдд");
-        form.$("[data-test-id=phone] input").setValue("+70000000000");
-        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=login] input").setValue("");
+        form.$("[data-test-id=password] input").setValue("");
         form.$(".button").click();
-        $("[data-test-id=order-success]").shouldHave(exactText("  Ваша заявка успешно отправлена! " +
-                "Наш менеджер свяжется с вами в ближайшее время."));*/
-
+        $("[data-test-id=login] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=password] span.input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    @DisplayName("Поля заполнены неверно")
+    void shouldErrorTestOfLogOrPass() {
+        open(WEBSITE);
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=login] input").setValue("fghfhjhj");
+        form.$("[data-test-id=password] input").setValue("123");
+        form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Неверно указан логин или пароль"));
+    }
+    @Test
+    @DisplayName("Одно из полей заполнено неверно")
+    void shouldErrorTestOfPass() {
+        open(WEBSITE);
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=login] input").setValue("vasya");
+        form.$("[data-test-id=password] input").setValue("qwerty123");
+        form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Неверно указан логин или пароль"));
+    }
+    @Test
+    @DisplayName("Поле 'код' заполнено неверно")
+    void shouldErrorTestOfCode() {
+        open(WEBSITE);
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=login] input").setValue("vasya");
+        form.$("[data-test-id=password] input").setValue("qwerty123");
+        form.$(".button").click();
+        form.$("[data-test-id=code] input").setValue("88888");
+        form.$(".button").click();
+        $("[data-test-id=error-notification]").shouldHave(text("Неверно указан код! Попробуйте ещё раз."));
     }
 }
